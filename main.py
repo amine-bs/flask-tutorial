3de6763e6c1bb79273c9b7b6c0e148be1511ce47
Ben Salha Mohamed Amine <mohamed-amine.ben-salha@student-cs.fr>
2022-09-20 19:29:18 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+from flask import Flask, render_template, request
+from utils import load_device, load_model, predict, load_image
+from PIL import Image
+import os
+
+
+
+def read_image(file):
+    img = Image.open(BytesIO(file))
+    return img
+
+app = Flask(__name__)
+
+device = load_device()
+model = load_model(device)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template('home.html')
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict_flask():
+    if request.method == "POST":
+        file = request.files['file']
+        filename = file.filename
+        file_path = os.path.join('static', filename)
+        file.save(file_path)
+        img = load_image(file_path)
+        pred = predict(img, model, device)
+    return render_template("predict.html", output=pred, user_image = file_path)
