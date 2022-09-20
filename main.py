3cebf05a1afdce9d0009e5b8d88dd43e1d817424
amine-bs <75427994+amine-bs@users.noreply.github.com>
2022-09-20 22:37:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ def read_image(file):
 
 app = Flask(__name__)
 
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 device = load_device()
 model = load_model(device)
