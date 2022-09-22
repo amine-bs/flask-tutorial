d6e1543d94b0eb72266eaa4f5e402466427e0edf
Ben Salha Mohamed Amine <mohamed-amine.ben-salha@student-cs.fr>
2022-09-22 09:40:42 +0100

    fix post

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template, request
 from utils import load_device, load_model, predict, load_image
 from PIL import Image
 import os
+import base64
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 def read_image(file):
-    img = Image.open(BytesIO(file))
+    img = Image.open(BytesIO(file)).convert("RGB")
     return img
 
 app = Flask(__name__)
@@ -26,9 +27,20 @@ def home():
 def predict_flask():
     if request.method == "POST":
         file = request.files['file']
+        img = read_image(file.read())
+
+        data = BytesIO()
+        img.save(data, "JPEG")
+        encoded_img_data = base64.b64encode(data.getvalue())
+        img_data=encoded_img_data.decode('utf-8')
+        """
         filename = file.filename
         file_path = os.path.join('static', filename)
         file.save(file_path)
         img = load_image(file_path)
+        """
         pred = predict(img, model, device)
+    return render_template("predict.html", output=pred, img_data=img_data)
+    """
     return render_template("predict.html", output=pred, user_image = file_path)
+    """
